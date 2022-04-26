31042a46b05278321d2690e38c217e6ea4cc015c
TrongDuc <72551721+duclt11@users.noreply.github.com>
2022-04-26 08:28:05 +0700

    Added feature: Attack

--- PandaGame/Assets/Script/Player/Attack.cs
+++ PandaGame/Assets/Script/Player/Attack.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    public State statePlayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,24 @@ public class Attack : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.CompareTag("Enemy"))
+        {
+            if (statePlayer.state != State.StatePlayer.Attack)
+            {
+                Debug.Log("Die");
+            }
+            else
+            {
+                if (collision.transform.localScale.x < -1)
+                {
+                    collision.transform.position = new Vector3(collision.transform.position.x - collision.transform.position.x / 10, collision.transform.position.y, collision.transform.position.z);
+                }
+                else
+                    collision.transform.position = new Vector3(collision.transform.position.x + collision.transform.position.x / 10, collision.transform.position.y, collision.transform.position.z);
+
+                collision.GetComponent<Rigidbody2D>().gravityScale = 2.0f;
+            }
+        }
         
         
     }
